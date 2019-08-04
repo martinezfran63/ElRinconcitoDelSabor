88daa56dbbf5383945db34c0a05c576a6fd2009d
Francisco <fmartinez@weedmaps.com>
2019-08-03 21:57:13 -0600

    prettier changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home.jsx";
 import Menu_One from "./Menu_One.jsx";
 import Menu_Two from "./Menu_Two.jsx";
 import Menu_Three from "./Menu_Three.jsx";
 import Lost from "./Lost.jsx";
 
-
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <Switch> 
+        <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/menu_one" component={Menu_One} />
           <Route path="/menu_two" component={Menu_Two} />
@@ -23,8 +18,6 @@ class App extends React.Component {
           <Route component={Lost} />
         </Switch>
       </Router>
-
-
     );
   }
 }
