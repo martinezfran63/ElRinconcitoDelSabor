7de754f98541d824ceb0bf29fb28ce0f669e21d4
Francisco <fmartinez@weedmaps.com>
2019-08-03 22:34:03 -0600

    fix buttons

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,7 @@ import Button from "@material-ui/core/Button";
 import { Segment } from "semantic-ui-react";
 import styled from "styled-components";
 import rinconcito_background from "./images/rinconcito_background_transparent.jpg";
+import { Link } from "react-router-dom";
 
 const Content = styled.div`
   background-image: url(${rinconcito_background});
@@ -16,12 +17,37 @@ class Home extends React.Component {
     return (
       <Content>
         <h1>El Rinconcito Del Sabor</h1>
-        <Button variant="contained">Menu One</Button>
-        <Button variant="contained" color="primary">
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/menu_one"
+        >
+          Menu One
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/menu_two"
+        >
           Menu Two
         </Button>
-        <Button variant="contained" color="secondary">
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/menu_three"
+        >
           Menu Three
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/menu_four"
+        >
+          Menu Four
         </Button>
       </Content>
     );
