3408359d1b56550433b540488101540333d73949
Francisco <fmartinez@weedmaps.com>
2019-08-03 22:24:12 -0600

    add styled components

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
-import { Header, Button, Grid, Image, Embed, Divider } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { Segment } from "semantic-ui-react";
+import styled from "styled-components";
+import rinconcito_background from "./images/rinconcito_background_transparent.jpg";
 
-export default class Home extends Component {
-    render(){
-        return(
-            <div>Jme
-                1
-            </div>
-                        
-        )
-    }
+const Content = styled.div`
+  background-image: url(${rinconcito_background});
+  width: 100%;
+  height: 2000px;
+`;
 
-
+class Home extends React.Component {
+  render() {
+    return (
+      <Content>
+        <h1>El Rinconcito Del Sabor</h1>
+        <Button variant="contained">Menu One</Button>
+        <Button variant="contained" color="primary">
+          Menu Two
+        </Button>
+        <Button variant="contained" color="secondary">
+          Menu Three
+        </Button>
+      </Content>
+    );
+  }
 }
+
+export default Home;
